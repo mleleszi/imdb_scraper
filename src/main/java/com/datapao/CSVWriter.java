dd1afb7593c69454e0b88f7a74db894f82e338c9
mleleszi <mleleszi@icloud.com>
2022-10-20 22:20:08 +0200

    added header printing to CSVWriter

--- src/main/java/com/datapao/CSVWriter.java
+++ src/main/java/com/datapao/CSVWriter.java
@@ -3,8 +3,6 @@ package com.datapao;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CSVWriter {
@@ -15,9 +13,10 @@ public class CSVWriter {
         this.filePath = filePath;
     }
 
-    public void write(List<String> lines) {
+    public void write(List<String> lines, String header) {
         File csvOutputFile = new File(filePath);
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            pw.println(header);
             lines.forEach(pw::println);
         } catch (IOException e) {
             e.printStackTrace();
